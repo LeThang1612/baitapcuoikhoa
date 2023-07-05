e9703380b8a95450ee94ab1b867433ac8182f722
LeThang1612 <126088173+LeThang1612@users.noreply.github.com>
2023-07-05 12:17:25 +0700

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// import NavigateHome from "./Screens/Lesson 10/NavigateHome";
+// import Lesson11 from "./Screens/Lesson 11/ProductTable";
+// import Lesson9 from "./Screens/Lesson 9/Lesson 9";
+// import ThemeContext from "./ThemeContext";
+// import "./Style/CountNumber.css";
+// import CountNumber from "./Screens/Lesson 4/CountNumber";
+// import TrafficLight from "./Screens/Lesson 5/TrafficLight";
+// import "./Style/LightButton.css";
+// import "./Style/FormRegister.css";
+// import Register from "./Screens/Lesson 3/Register";
+// import Contacts from "./Screens/Practice lessons/Contacts";
+import TodoList from "./Screens/FinalExam_Trang/FinalExam";
+import "./Screens/FinalExam_Trang/FinalExam.css";
+
+function App() {
+  // return <CountNumber />;npm
+  // return <TrafficLight />;
+  // return <Register />;
+  // return <table />;
+  // return <Lesson9 />;
+  // return <NavigateHome />;
+  // return <Lesson11 />;
+  // return <Contacts />;
+  return <TodoList />;
+}
+
+export default App;
